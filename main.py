999f943faadf9c29d77e3d4b6833704c69af3697
RehabElb <63158011+RehabElb@users.noreply.github.com>
2022-03-02 01:50:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#print("hello print")
+
+
+# my_name = "Rehab"
+
+#how to print an aoutput to the terminal
+print("Rehab")
+
+#how to declare or initiate a variable
+#name_of_the_variable = value
+name = "Rehab"
+age = 7
+instructor = True
+credits = 12.00
+
+if instructor:
+  print("Yes!")
+
+an_int=2
+a_float=2.2
+
+sum=an_int + a_float
+divisions = a_float/an_int
+modules=an_int % a_float
+
+#print(divisions)
+decide=int(divisions)
+#print(decide)
+#print(modules)
+#print(sum)
+if decide == 1.0:
+  print("go out and the decision value is " + str(decide))
+else:
+  print("stay at home")
+
+#check the type of the data
+an_integ=6
+print(type(an_integ))
+
+a_float=6.80
+print(type(a_float))
+
+a_bool=False
+print(type(a_bool))
+
+a_string= "Rehab"
+print(type(a_string))
+
+first= "hello "
+second="world"
+greetings= first + second
+#print(greetings)
+
+
+print(int(25*68+13/28))
+
+print(6**2)
+print(2**4)
+
